c30a43df544e0092fccdf0654dbfeb6e33e19744
caochanhduong <caochanhduongbku@gmail.com>
2020-10-31 09:04:18 +0700

    update 605

--- leetcode_605.go
+++ leetcode_605.go
@@ -1,18 +1,18 @@
 func canPlaceFlowers(flowerbed []int, n int) bool {
-    length := len(flowerbed)
-    if length == 1 && flowerbed[0] == 0 || n == 0 {
+    if n == 0 {
         return true
     }
     
-    for i := 0; i < length; i++ {
-        if flowerbed[i] == 0 && ((i == 0  && i < length - 1 && flowerbed[i+1] == 0) || (i == length - 1 && i - 1 >= 0 && flowerbed[i-1] == 0) || (i - 1 >= 0 && i < length - 1 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0)) {
-            flowerbed[i] = 1
-            n--
-            if n == 0 {
-                return true
+    for i, v := range flowerbed {
+        if v == 0 {
+            if (i == 0 || flowerbed[i-1] == 0) && (i == len(flowerbed) - 1 || flowerbed[i+1] == 0) {
+                flowerbed[i] = 1
+                n--
+                if n == 0 {
+                    return true
+                }
             }
         }
     }
-        
     return false
 }
